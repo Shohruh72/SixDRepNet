b1d925dc54554b8154e7d1fbd95f0d8ee48bc73e
Shohruh Rakhmatov <yusuf.proai@gmail.com>
2025-08-04 10:37:47 +0500

    Update util.py

--- utils/util.py
+++ utils/util.py
@@ -1,8 +1,11 @@
 # https://github.com/Shohruh72
+import os
+import cv2
 import math
 import random
 import numpy as np
 from PIL import Image
+from math import sin, cos
 
 import torch
 import torchvision.transforms as T
@@ -121,10 +124,6 @@ def compute_euler(rotation_matrices):
     return out_euler
 
 
-def resample():
-    return random.choice((Image.BILINEAR, Image.BICUBIC))
-
-
 class Resize:
     def __init__(self, size: int):
         self.size = size
@@ -133,7 +132,7 @@ class Resize:
         size = self.size
         i, j, h, w = self.params(image.size)
         image = image.crop((j, i, j + w, i + h))
-        return image.resize([size, size], resample())
+        return image.resize([size, size], self.resample())
 
     @staticmethod
     def params(size):
@@ -164,6 +163,10 @@ class Resize:
         j = (size[0] - w) // 2
         return i, j, h, w
 
+    @staticmethod
+    def resample():
+        return random.choice((Image.BILINEAR, Image.BICUBIC))
+
 
 def get_transforms(args, for_training=True):
     if for_training:
@@ -179,51 +182,6 @@ def get_transforms(args, for_training=True):
             T.ToTensor(),
             T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
-
-
-def plot_pose_cube(img, yaw, pitch, roll, tdx=None, tdy=None, size=150.):
-    import cv2
-    import numpy as np
-    from math import sin, cos
-    p = pitch * np.pi / 180
-    y = -(yaw * np.pi / 180)
-    r = roll * np.pi / 180
-    if tdx != None and tdy != None:
-        face_x = tdx - 0.50 * size
-        face_y = tdy - 0.50 * size
-
-    else:
-        height, width = img.shape[:2]
-        face_x = width / 2 - 0.5 * size
-        face_y = height / 2 - 0.5 * size
-
-    x1 = size * (cos(y) * cos(r)) + face_x
-    y1 = size * (cos(p) * sin(r) + cos(r) * sin(p) * sin(y)) + face_y
-    x2 = size * (-cos(y) * sin(r)) + face_x
-    y2 = size * (cos(p) * cos(r) - sin(p) * sin(y) * sin(r)) + face_y
-    x3 = size * (sin(y)) + face_x
-    y3 = size * (-cos(y) * sin(p)) + face_y
-
-    # Draw base in red
-    cv2.line(img, (int(face_x), int(face_y)), (int(x1), int(y1)), (0, 0, 255), 3)
-    cv2.line(img, (int(face_x), int(face_y)), (int(x2), int(y2)), (0, 0, 255), 3)
-    cv2.line(img, (int(x2), int(y2)), (int(x2 + x1 - face_x), int(y2 + y1 - face_y)), (0, 0, 255), 3)
-    cv2.line(img, (int(x1), int(y1)), (int(x1 + x2 - face_x), int(y1 + y2 - face_y)), (0, 0, 255), 3)
-    # Draw pillars in blue
-    cv2.line(img, (int(face_x), int(face_y)), (int(x3), int(y3)), (255, 0, 0), 2)
-    cv2.line(img, (int(x1), int(y1)), (int(x1 + x3 - face_x), int(y1 + y3 - face_y)), (255, 0, 0), 2)
-    cv2.line(img, (int(x2), int(y2)), (int(x2 + x3 - face_x), int(y2 + y3 - face_y)), (255, 0, 0), 2)
-    cv2.line(img, (int(x2 + x1 - face_x), int(y2 + y1 - face_y)),
-             (int(x3 + x1 + x2 - 2 * face_x), int(y3 + y2 + y1 - 2 * face_y)), (255, 0, 0), 2)
-    # Draw top in green
-    cv2.line(img, (int(x3 + x1 - face_x), int(y3 + y1 - face_y)),
-             (int(x3 + x1 + x2 - 2 * face_x), int(y3 + y2 + y1 - 2 * face_y)), (0, 255, 0), 2)
-    cv2.line(img, (int(x2 + x3 - face_x), int(y2 + y3 - face_y)),
-             (int(x3 + x1 + x2 - 2 * face_x), int(y3 + y2 + y1 - 2 * face_y)), (0, 255, 0), 2)
-    cv2.line(img, (int(x3), int(y3)), (int(x3 + x1 - face_x), int(y3 + y1 - face_y)), (0, 255, 0), 2)
-    cv2.line(img, (int(x3), int(y3)), (int(x3 + x2 - face_x), int(y3 + y2 - face_y)), (0, 255, 0), 2)
-
-    return img
 
 
 class AverageMeter:
@@ -277,3 +235,268 @@ class GeodesicLoss(torch.nn.Module):
         theta = torch.acos(torch.clamp(cos, -1 + self.eps, 1 - self.eps))
 
         return torch.mean(theta)
+
+
+def distance2box(points, distance, max_shape=None):
+    x1 = points[:, 0] - distance[:, 0]
+    y1 = points[:, 1] - distance[:, 1]
+    x2 = points[:, 0] + distance[:, 2]
+    y2 = points[:, 1] + distance[:, 3]
+    if max_shape is not None:
+        x1 = x1.clamp(min=0, max=max_shape[1])
+        y1 = y1.clamp(min=0, max=max_shape[0])
+        x2 = x2.clamp(min=0, max=max_shape[1])
+        y2 = y2.clamp(min=0, max=max_shape[0])
+    return np.stack([x1, y1, x2, y2], axis=-1)
+
+
+class FaceDetector:
+    def __init__(self, onnx_path=None, session=None):
+        from onnxruntime import InferenceSession
+        self.session = session
+
+        self.batched = False
+        if self.session is None:
+            assert onnx_path is not None
+            assert os.path.exists(onnx_path)
+            self.session = InferenceSession(onnx_path,
+                                            providers=['CUDAExecutionProvider'])
+        self.nms_thresh = 0.4
+        self.center_cache = {}
+        input_cfg = self.session.get_inputs()[0]
+        input_shape = input_cfg.shape
+        if isinstance(input_shape[2], str):
+            self.input_size = None
+        else:
+            self.input_size = tuple(input_shape[2:4][::-1])
+        input_name = input_cfg.name
+        outputs = self.session.get_outputs()
+        if len(outputs[0].shape) == 3:
+            self.batched = True
+        output_names = []
+        for output in outputs:
+            output_names.append(output.name)
+        self.input_name = input_name
+        self.output_names = output_names
+        self.use_kps = False
+        self._num_anchors = 1
+        if len(outputs) == 6:
+            self.fmc = 3
+            self._feat_stride_fpn = [8, 16, 32]
+            self._num_anchors = 2
+        elif len(outputs) == 9:
+            self.fmc = 3
+            self._feat_stride_fpn = [8, 16, 32]
+            self._num_anchors = 2
+            self.use_kps = True
+        elif len(outputs) == 10:
+            self.fmc = 5
+            self._feat_stride_fpn = [8, 16, 32, 64, 128]
+            self._num_anchors = 1
+        elif len(outputs) == 15:
+            self.fmc = 5
+            self._feat_stride_fpn = [8, 16, 32, 64, 128]
+            self._num_anchors = 1
+            self.use_kps = True
+
+    def forward(self, x, score_thresh):
+        scores_list = []
+        bboxes_list = []
+        points_list = []
+        input_size = tuple(x.shape[0:2][::-1])
+        blob = cv2.dnn.blobFromImage(x,
+                                     1.0 / 128,
+                                     input_size,
+                                     (127.5, 127.5, 127.5), swapRB=True)
+        outputs = self.session.run(self.output_names, {self.input_name: blob})
+        input_height = blob.shape[2]
+        input_width = blob.shape[3]
+        fmc = self.fmc
+        for idx, stride in enumerate(self._feat_stride_fpn):
+            if self.batched:
+                scores = outputs[idx][0]
+                boxes = outputs[idx + fmc][0]
+                boxes = boxes * stride
+            else:
+                scores = outputs[idx]
+                boxes = outputs[idx + fmc]
+                boxes = boxes * stride
+
+            height = input_height // stride
+            width = input_width // stride
+            key = (height, width, stride)
+            if key in self.center_cache:
+                anchor_centers = self.center_cache[key]
+            else:
+                anchor_centers = np.stack(np.mgrid[:height, :width][::-1], axis=-1)
+                anchor_centers = anchor_centers.astype(np.float32)
+
+                anchor_centers = (anchor_centers * stride).reshape((-1, 2))
+                if self._num_anchors > 1:
+                    anchor_centers = np.stack([anchor_centers] * self._num_anchors, axis=1)
+                    anchor_centers = anchor_centers.reshape((-1, 2))
+                if len(self.center_cache) < 100:
+                    self.center_cache[key] = anchor_centers
+
+            pos_indices = np.where(scores >= score_thresh)[0]
+            bboxes = distance2box(anchor_centers, boxes)
+            pos_scores = scores[pos_indices]
+            pos_bboxes = bboxes[pos_indices]
+            scores_list.append(pos_scores)
+            bboxes_list.append(pos_bboxes)
+        return scores_list, bboxes_list
+
+    def detect(self, image, input_size=None, score_threshold=0.5, max_num=0, metric='default'):
+        assert input_size is not None or self.input_size is not None
+        input_size = self.input_size if input_size is None else input_size
+        image_ratio = float(image.shape[0]) / image.shape[1]
+        model_ratio = float(input_size[1]) / input_size[0]
+        if image_ratio > model_ratio:
+            new_height = input_size[1]
+            new_width = int(new_height / image_ratio)
+        else:
+            new_width = input_size[0]
+            new_height = int(new_width * image_ratio)
+        det_scale = float(new_height) / image.shape[0]
+        resized_img = cv2.resize(image, (new_width, new_height))
+        det_img = np.zeros((input_size[1], input_size[0], 3), dtype=np.uint8)
+        det_img[:new_height, :new_width, :] = resized_img
+
+        scores_list, bboxes_list = self.forward(det_img, score_threshold)
+
+        scores = np.vstack(scores_list)
+        scores_ravel = scores.ravel()
+        order = scores_ravel.argsort()[::-1]
+        bboxes = np.vstack(bboxes_list) / det_scale
+        pre_det = np.hstack((bboxes, scores)).astype(np.float32, copy=False)
+        pre_det = pre_det[order, :]
+        keep = self.nms(pre_det)
+        det = pre_det[keep, :]
+        if 0 < max_num < det.shape[0]:
+            area = (det[:, 2] - det[:, 0]) * (det[:, 3] - det[:, 1])
+            img_center = image.shape[0] // 2, image.shape[1] // 2
+            offsets = np.vstack([(det[:, 0] + det[:, 2]) / 2 - img_center[1],
+                                 (det[:, 1] + det[:, 3]) / 2 - img_center[0]])
+            offset_dist_squared = np.sum(np.power(offsets, 2.0), 0)
+            if metric == 'max':
+                values = area
+            else:
+                values = area - offset_dist_squared * 2.0  # some extra weight on the centering
+            index = np.argsort(values)[::-1]  # some extra weight on the centering
+            index = index[0:max_num]
+            det = det[index, :]
+        return det
+
+    def nms(self, outputs):
+        thresh = self.nms_thresh
+        x1 = outputs[:, 0]
+        y1 = outputs[:, 1]
+        x2 = outputs[:, 2]
+        y2 = outputs[:, 3]
+        scores = outputs[:, 4]
+
+        order = scores.argsort()[::-1]
+        areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+
+        keep = []
+        while order.size > 0:
+            i = order[0]
+            keep.append(i)
+            xx1 = np.maximum(x1[i], x1[order[1:]])
+            yy1 = np.maximum(y1[i], y1[order[1:]])
+            xx2 = np.minimum(x2[i], x2[order[1:]])
+            yy2 = np.minimum(y2[i], y2[order[1:]])
+
+            w = np.maximum(0.0, xx2 - xx1 + 1)
+            h = np.maximum(0.0, yy2 - yy1 + 1)
+            inter = w * h
+            ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+            indices = np.where(ovr <= thresh)[0]
+            order = order[indices + 1]
+
+        return keep
+
+
+def plot_pose_cube(img, yaw, pitch, roll, tdx=None, tdy=None, size=150.0):
+    p = pitch * np.pi / 180
+    y = -(yaw * np.pi / 180)
+    r = roll * np.pi / 180
+    if tdx is not None and tdy is not None:
+        face_x = tdx - 0.50 * size
+        face_y = tdy - 0.50 * size
+    else:
+        height, width = img.shape[:2]
+        face_x = width / 2 - 0.5 * size
+        face_y = height / 2 - 0.5 * size
+
+    x1 = size * (cos(y) * cos(r)) + face_x
+    y1 = size * (cos(p) * sin(r) + cos(r) * sin(p) * sin(y)) + face_y
+    x2 = size * (-cos(y) * sin(r)) + face_x
+    y2 = size * (cos(p) * cos(r) - sin(p) * sin(y) * sin(r)) + face_y
+    x3 = size * (sin(y)) + face_x
+    y3 = size * (-cos(y) * sin(p)) + face_y
+
+    # Draw base in red
+    cv2.line(img, (int(face_x), int(face_y)), (int(x1), int(y1)), (0, 0, 255), 3)
+    cv2.line(img, (int(face_x), int(face_y)), (int(x2), int(y2)), (0, 0, 255), 3)
+    cv2.line(img, (int(x2), int(y2)), (int(x2 + x1 - face_x), int(y2 + y1 - face_y)), (0, 0, 255), 3)
+    cv2.line(img, (int(x1), int(y1)), (int(x1 + x2 - face_x), int(y1 + y2 - face_y)), (0, 0, 255), 3)
+    # Draw pillars in blue
+    cv2.line(img, (int(face_x), int(face_y)), (int(x3), int(y3)), (255, 0, 0), 2)
+    cv2.line(img, (int(x1), int(y1)), (int(x1 + x3 - face_x), int(y1 + y3 - face_y)), (255, 0, 0), 2)
+    cv2.line(img, (int(x2), int(y2)), (int(x2 + x3 - face_x), int(y2 + y3 - face_y)), (255, 0, 0), 2)
+    cv2.line(img, (int(x2 + x1 - face_x), int(y2 + y1 - face_y)),
+             (int(x3 + x1 + x2 - 2 * face_x), int(y3 + y2 + y1 - 2 * face_y)), (255, 0, 0), 2)
+    # Draw top in green
+    cv2.line(img, (int(x3 + x1 - face_x), int(y3 + y1 - face_y)),
+             (int(x3 + x1 + x2 - 2 * face_x), int(y3 + y2 + y1 - 2 * face_y)), (0, 255, 0), 2)
+    cv2.line(img, (int(x2 + x3 - face_x), int(y2 + y3 - face_y)),
+             (int(x3 + x1 + x2 - 2 * face_x), int(y3 + y2 + y1 - 2 * face_y)), (0, 255, 0), 2)
+    cv2.line(img, (int(x3), int(y3)), (int(x3 + x1 - face_x), int(y3 + y1 - face_y)), (0, 255, 0), 2)
+    cv2.line(img, (int(x3), int(y3)), (int(x3 + x2 - face_x), int(y3 + y2 - face_y)), (0, 255, 0), 2)
+
+    # Convert tensor inputs to float
+    yaw_val = float(yaw.item()) if hasattr(yaw, 'item') else float(yaw)
+    pitch_val = float(pitch.item()) if hasattr(pitch, 'item') else float(pitch)
+    roll_val = float(roll.item()) if hasattr(roll, 'item') else float(roll)
+
+    # Define colors for info panel
+    colors = [(0, 255, 0), (0, 0, 255), (255, 0, 0)]  # Example colors for yaw, pitch, roll
+
+    # Add info panel with converted values
+    info_panel(img, yaw_val, pitch_val, roll_val, colors)
+
+    return img
+
+
+def info_panel(img, yaw, pitch, roll, colors):
+    w, h = 200, 110
+    x, y = 900, 10
+    y_c, p_c, r_c = colors
+
+    overlay = img.copy()
+    cv2.rectangle(overlay, (x, y), (x + w, y + h), (20, 20, 20), -1)
+    cv2.rectangle(overlay, (x, y), (x + w, y + h), (100, 200, 255), 1)
+    cv2.addWeighted(overlay, 0.7, img, 0.3, 0, img)
+
+    glow_overlay = img.copy()
+    cv2.rectangle(glow_overlay, (x - 2, y - 2), (x + w + 2, y + h + 2), (100, 200, 255), 1)
+    cv2.addWeighted(glow_overlay, 0.3, img, 0.7, 0, img)
+
+    cv2.putText(img, "HEAD POSE ANGLES", (x + 10, y + 25),
+                cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.7, (255, 255, 255), 1, cv2.LINE_AA)
+
+    angles_data = [(f"YAW:   {yaw:6.1f}", y_c, "←→"),
+                   (f"PITCH: {pitch:6.1f}", p_c, "↑↓"),
+                   (f"ROLL:  {roll:6.1f}", r_c, "↻↺")]
+
+    for i in range(1, 3):
+        y_div = y + 50 + i * 25 - 15
+        cv2.line(img, (x + 10, y_div), (x + w - 10, y_div), (100, 100, 100), 1, cv2.LINE_AA)
+
+    for i, (text, color, symbol) in enumerate(angles_data):
+        y_pos = y + 50 + i * 25
+        cv2.circle(img, (x + 15, y_pos - 5), 6, color, -1)
+        cv2.circle(img, (x + 15, y_pos - 5), 6, (255, 255, 255), 1)
+        cv2.putText(img, text, (x + 30, y_pos), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.6, (255, 255, 255), 1, cv2.LINE_AA)
